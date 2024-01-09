dee53df453edbdd25840f34c16dbfcab839ca938
Lavanya Lankalapilli <104729497+lavanya-lankalapilli@users.noreply.github.com>
2024-01-09 10:46:08 -0800

    Update NumberOfIslands.java

--- LeetCodeProblems-main/src/TopInterviewQuestions/NumberOfIslands.java
+++ LeetCodeProblems-main/src/TopInterviewQuestions/NumberOfIslands.java
@@ -1,34 +1,30 @@
 package TopInterviewQuestions;
 
 class Solution {
-    private boolean[][] visited;
-    private int count = 0;
-
     public int numIslands(char[][] grid) {
-        visited = new boolean[grid.length][grid[0].length];
+        boolean visited[][] = new boolean[grid.length][grid[0].length];
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
-                    visit(grid, i, j);
+                if (!visited[i][j] && grid[i][j] == '1') {
                     count++;
+                    visit(visited, grid, i, j);
                 }
             }
         }
         return count;
     }
 
-    private void visit(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0'
-                || visited[i][j]) {
+    private void visit(boolean visited[][], char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || visited[i][j] || grid[i][j] == '0') {
             return;
         }
         visited[i][j] = true;
-        visit(grid, i, j - 1);
-        visit(grid, i - 1, j);
-        visit(grid, i, j + 1);
-        visit(grid, i + 1, j);
+        visit(visited, grid, i - 1, j);
+        visit(visited, grid, i, j - 1);
+        visit(visited, grid, i, j + 1);
+        visit(visited, grid, i + 1, j);
     }
-
 }
 
 public class NumberOfIslands {
