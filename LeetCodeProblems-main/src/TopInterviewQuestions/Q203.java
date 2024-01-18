81c4d56e9cfad7e005fb22349a4427880ff8f0b7
Lavanya Lankalapilli <104729497+lavanya-lankalapilli@users.noreply.github.com>
2024-01-18 12:23:09 -0800

    Create Q203.java

---
+++ LeetCodeProblems-main/src/TopInterviewQuestions/Q203.java
@@ -0,0 +1,16 @@
+public class Q203 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode prev = dummy;
+        while (head != null) {
+            if (head.val == val) {
+                prev.next = head.next;
+            } else {
+                prev = head;
+            }
+            head = head.next;
+        }
+        return dummy.next;
+    }
+}
