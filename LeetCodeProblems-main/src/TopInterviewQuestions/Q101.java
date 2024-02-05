4ef8e6b938cd6196c58113dcd870275f76e4c6ad
Lavanya Lankalapilli <104729497+lavanya-lankalapilli@users.noreply.github.com>
2024-02-05 09:16:33 -0800

    Create Q101.java

---
+++ LeetCodeProblems-main/src/TopInterviewQuestions/Q101.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Q101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null ^ node2 == null) {
+            return false;
+        }
+        if (node1.val != node2.val || !isSymmetric(node1.left, node2.right) ||
+                !isSymmetric(node1.right, node2.left)) {
+            return false;
+        }
+        return true;
+    }
+  public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+        System.out.println(new Q101().isSymmetric(root));
+
+    }
+}
