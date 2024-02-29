737c880ba1208df57bb5cf78cae044298469bf0c
Lavanya Lankalapilli <104729497+lavanya-lankalapilli@users.noreply.github.com>
2024-02-29 15:11:30 -0800

    Create Q88.java

---
+++ LeetCodeProblems-main/src/TopInterviewQuestions/Q88.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        for (int i = m + n - 1; i >= 0; i--) {
+            if (p2 < 0) {
+                nums1[i] = nums1[p1--];
+            } else if (p1 < 0) {
+                nums1[i] = nums2[p2--];
+            } else if (nums1[p1] > nums2[p2]) {
+                nums1[i] = nums1[p1--];
+            } else {
+                nums1[i] = nums2[p2--];
+            }
+        }
+    }
+}
