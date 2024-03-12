6b30c8808e0585dce3140bd84fb84b7dabc4dd7a
Lavanya Lankalapilli <104729497+lavanya-lankalapilli@users.noreply.github.com>
2024-03-12 13:36:07 -0700

    Create Q1827.java

---
+++ LeetCodeProblems-main/src/TopInterviewQuestions/Q1827.java
@@ -0,0 +1,13 @@
+class Solution {
+     public int minOperations(int[] nums) {
+        int res = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                int temp = nums[i - 1] - nums[i ] + 1;
+                res += temp;
+                nums[i] += temp;
+            }
+        }
+        return res;
+    }
+}
